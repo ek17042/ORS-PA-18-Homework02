335c6840edecf3a5d9445ad744cfc919f56d16b4
Edona Kallaba <edona.kallaba@udg.edu.me>
2019-02-28 13:44:21 +0100

    task1.py

--- task1.py
+++ task1.py
@@ -1,25 +1,39 @@
-"""
-===================   TASK 1   ====================
-* Name: Can String Be Float
-*
-* Write a function `can_string_be_float` that will
-* check whether the passed string value can be
-* converted to float. If the string value can be
-* successfully converted to float, function should
-* return `True` otherwise it should return `False`.
-*
-* Note: Please describe in details possible cases
-* in which your solution might not work.
-*
-* Use main() function to test your solution.
-===================================================
-"""
+def can_string_be_float(user_value):
 
+    allowed_characters = ["0","1","2","3","4","5","6","7","8","9",".","-"]
 
+    for x in user_value:
+        if x not in allowed_characters:
+            return False
+
+    nr_of_dots = 0
+
+    for x in user_value:
+        if x == ".":
+            nr_of_dots = nr_of_dots + 1
+
+    if nr_of_dots > 1:
+        return False
+
+    nr_of_minuses = 0
+
+    for x in user_value:
+        if x == "-":
+            nr_of_minuses = nr_of_minuses + 1
+
+    if nr_of_minuses > 1:
+        return False
+
+    if nr_of_minuses == 1:
+        if user_value[0] != "-":
+            return False
+
+    return True
 def main():
-
     user_value = input("Enter string which will be evaluated: ")
-    print(can_string_be_float(user_value))
-    print(float(user_value))
-
+    if can_string_be_float(user_value):
+        print(float(user_value))
+    else:
+        print("String cannot be onverted to float!")
+    #print(float(user_value))
 main()
